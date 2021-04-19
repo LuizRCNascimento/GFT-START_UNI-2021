8fb06f22f4242005ce3cf65a30728485220fd6cf
LuizRCNascimento <eng.lnascimento@gmail.com>
2021-04-19 11:08:02 -0300

    updates after training

--- desafio2/Caminhao.java
+++ desafio2/Caminhao.java
@@ -1,6 +1,6 @@
 package desafio2;
 
-public class Caminhao extends Transporte {
+public class Caminhao extends Carga implements Transporte {
     @Override
     public void calculaFrete() {
         double frete = getPeso() *0.12 + getValor()*0.3;
